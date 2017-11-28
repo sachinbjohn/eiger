ed7d6af1b06c15f32460d00ae8bc3e0276251f4c
root <root@dco-node049.dco.ethz.ch>
2017-11-28 23:02:41 +0100

    Changed permissions

--- kill_all.bash
+++ kill_all.bash
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 nservers=$1
-dcl_config=${nservers}_in_vicci
-client_config=${nservers}_clients_in_vicci
+dcl_config=vicci_dcl_config/${nservers}_in_vicci
+client_config=vicci_dcl_config/${nservers}_clients_in_vicci
 
 ./vicci_stress_killer.bash $client_config
 ./vicci_cassandra_killer.bash $dcl_config
